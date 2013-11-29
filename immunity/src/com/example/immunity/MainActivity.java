a0af957a5e5fb46f64f870fe7359ec482fe1364d
Alexander <itman91@gmail.com>
2013-11-29 19:26:33 +0000

    Revert "My First Commit"
    This reverts commit 95ee82d499ac2955b29f28b366b499e00b4c2fe8.

--- immunity/src/com/example/immunity/MainActivity.java
+++ immunity/src/com/example/immunity/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("GIT", "I think this shoud work now");
     }
 
 
